3818986cdc12f753bccbdedb6a90dafe3f5736ed
Daily dose of hacking <52329418+TTVralph@users.noreply.github.com>
2026-07-09 12:15:09 -0600

    Fix Compose state delegate import

--- MiruroApp/src/main/kotlin/com/ttvralph/miruroapp/MainActivity.kt
+++ MiruroApp/src/main/kotlin/com/ttvralph/miruroapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
